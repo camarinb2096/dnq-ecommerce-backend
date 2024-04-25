0205e2bc32afec6b04ee461a642719ff31f8b740
camilo_marin <camarinb20@gmail.com>
2024-04-24 20:25:09 -0500

    define auth component

--- internal/auth/services.go
+++ internal/auth/services.go
@@ -1,1 +1,27 @@
 package auth
+
+import (
+	"cmarin20/dnq-ecommerce/internal/user"
+	"cmarin20/dnq-ecommerce/pkg/logger"
+)
+
+type Services interface {
+	Login(data interface{}) (string, error)
+}
+
+type services struct {
+	repo   user.Repository
+	logger *logger.Logger
+}
+
+func NewService(repo user.Repository, logger *logger.Logger) Services {
+	return &services{
+		repo:   repo,
+		logger: logger,
+	}
+}
+
+func (s *services) Login(data interface{}) (string, error) {
+	s.logger.Info("Logging in...")
+	return "", nil
+}
