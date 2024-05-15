75721f8444d39bca0e35c886060a10dc1b7b0108
camilo_marin <camarinb20@gmail.com>
2024-05-14 21:10:04 -0500

    implement switch for specific error codes on login endpoint

--- internal/auth/endpoints.go
+++ internal/auth/endpoints.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	dtos "cmarin20/dnq-ecommerce/internal/dto"
+	"errors"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,20 +21,31 @@ func NewEndpoints(s Services) Endpoints {
 	}
 }
 
+var ErrUserNotFound = errors.New("user not found")
+
 func login(s Services) Controller {
 	return func(c *gin.Context) {
 		var data dtos.UserLogin
+
 		if err := c.ShouldBindJSON(&data); err != nil {
 			c.JSON(400, gin.H{"error": err.Error()})
 			return
 		}
-
 		userLoged, err := s.Login(data)
 		if err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
-			return
-		}
 
+			switch err.Error() {
+			case "user not found":
+				c.JSON(404, gin.H{"error": err.Error()})
+				return
+			case "invalid password":
+				c.JSON(401, gin.H{"error": err.Error()})
+				return
+			default:
+				c.JSON(400, gin.H{"error": err.Error()})
+				return
+			}
+		}
 		c.JSON(200, gin.H{
 			"message": "Login successful",
 			"data":    userLoged})
