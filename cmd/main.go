df820f61544e8a6cb7e11fa7179ac8119ba77f17
Alejandro Marin Bustamante <111027925+camarinb2096@users.noreply.github.com>
2024-11-14 01:19:59 +0000

    many changes

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"cmarin20/dnq-ecommerce/internal/di"
 	"log"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 	
 	app, err := di.Initialize()
 	if err != nil {
